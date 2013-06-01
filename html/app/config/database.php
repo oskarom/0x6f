fbaa767fff79d38f3b98284661d85a32ce90c0f7
Oskar Omarsson <oskarom@gmail.com>
2013-06-01 02:58:35 +0000

    more stuff

--- html/app/config/database.php
+++ html/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'db0x6f',
+			'username'  => 'db0x6f',
+			'password'  => 'db0x6f',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
